9f411030e42548d845feaf46177d430b2410e5b8
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-05-16 16:30:25 -0400

    add similarity analysis of LDA-LDA correlation across epochs

---
+++ SingleUnitAnalysis/SimilarityLDA_LDA.m
@@ -0,0 +1,92 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Similarity of PCA and LDA coefficient vectors as function of time
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath('../Func');
+setDir;
+
+cmap                = cbrewer('div', 'Spectral', 128, 'cubic');
+cmap                = flipud(cmap);
+numFold             = 30;
+numRandPickUnits    = 100;
+numTrials           = numRandPickUnits*6;
+totTargets          = [true(numTrials/2,1); false(numTrials/2,1)];
+ROCThres            = 0.5;
+
+
+load([TempDatDir 'Shuffle_Spikes.mat']);
+selectedNeuronalIndex = ActiveNeuronIndex';
+selectedNeuronalIndex = selectedHighROCneurons(nDataSet, params, ROCThres, selectedNeuronalIndex);
+nDataSet              = nDataSet(selectedNeuronalIndex);
+numUnits              = length(nDataSet);
+numT                  = length(params.timeSeries);
+corrMat               = nan(numFold, numT, numT);
+
+for nFold             = 1:numFold
+    currRandPickUnits     = numRandPickUnits;
+    nSessionData = shuffleSessionData(nDataSet(randperm(numUnits, currRandPickUnits)), totTargets, numTrials);
+    nSessionData = normalizationDim(nSessionData, 2);
+    coeffs       = coeffLDA(nSessionData, totTargets);
+    corrMat(nFold, :, :) = coeffs'*coeffs;
+end
+
+
+figure;
+hold on
+
+imagesc(params.timeSeries, params.timeSeries, squeeze(mean(corrMat, 1)));
+xlim([min(params.timeSeries) max(params.timeSeries)]);
+ylim([min(params.timeSeries) max(params.timeSeries)]);
+caxis([0 1]);
+axis xy;
+gridxy ([params.polein, params.poleout, 0],[params.polein, params.poleout, 0], 'Color','k','Linestyle','--','linewid', 0.5);
+box off;
+hold off;
+xlabel('LDA Time (s)')
+ylabel('LDA Time (s)')
+colormap(cmap)
+set(gca, 'TickDir', 'out')
+setPrint(8, 6, 'Plots/SimilarityLDALDA_100')
+
+
+numRandPickUnits    = 400;
+numTrials           = numRandPickUnits*6;
+totTargets          = [true(numTrials/2,1); false(numTrials/2,1)];
+ROCThres            = 0.5;
+
+
+load([TempDatDir 'Shuffle_Spikes.mat']);
+selectedNeuronalIndex = ActiveNeuronIndex';
+selectedNeuronalIndex = selectedHighROCneurons(nDataSet, params, ROCThres, selectedNeuronalIndex);
+nDataSet              = nDataSet(selectedNeuronalIndex);
+numUnits              = length(nDataSet);
+numT                  = length(params.timeSeries);
+corrMat               = nan(numFold, numT, numT);
+
+for nFold             = 1:numFold
+    currRandPickUnits     = numRandPickUnits;
+    nSessionData = shuffleSessionData(nDataSet(randperm(numUnits, currRandPickUnits)), totTargets, numTrials);
+    nSessionData = normalizationDim(nSessionData, 2);
+    coeffs       = coeffLDA(nSessionData, totTargets);
+    corrMat(nFold, :, :) = coeffs'*coeffs;
+end
+
+
+figure;
+hold on
+
+imagesc(params.timeSeries, params.timeSeries, squeeze(mean(corrMat, 1)));
+xlim([min(params.timeSeries) max(params.timeSeries)]);
+ylim([min(params.timeSeries) max(params.timeSeries)]);
+caxis([0 1]);
+axis xy;
+gridxy ([params.polein, params.poleout, 0],[params.polein, params.poleout, 0], 'Color','k','Linestyle','--','linewid', 0.5);
+box off;
+hold off;
+xlabel('LDA Time (s)')
+ylabel('LDA Time (s)')
+colormap(cmap)
+set(gca, 'TickDir', 'out')
+setPrint(8, 6, 'Plots/SimilarityLDALDA_400')
+
+setColorbar(cmap, 0, 1, 'similarity', 'Plots/SimilarityLDALDA')
