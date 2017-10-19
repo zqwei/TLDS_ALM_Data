379014bc82f6315e6e6bfd4490ecf85c9ba64d69
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-10-19 14:21:00 -0400

    add back fitting code

---
+++ LiSimultaneousRecordings/fitTLDS.m
@@ -0,0 +1,38 @@
+addpath('../Func');
+addpath('../Release_LDSI_v3')
+setDir;
+
+load([TempDatDir 'Simultaneous_Spikes.mat'])
+mean_type    = 'Constant_mean';
+tol          = 1e-3;
+cyc          = 10000;
+timePoint    = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+timePoint    = timePoint(2:end-1);
+numSession   = length(nDataSet);
+nFold        = 10;
+
+for nSession = 9:numSession
+    
+    Y          = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+    Y          = permute(Y, [2 3 1]);
+    xDim       = size(Y,1)-2;
+    
+    err_model1 = cross_valid_ldsi_uni(Y, xDim, nFold, timePoint, 'mean_type',mean_type,'tol',tol,'cyc',cyc,'is_fix_C',true,'is_fix_A',true);
+    err_model2 = cross_valid_ldsi_uni(Y, xDim, nFold, timePoint, 'mean_type',mean_type,'tol',tol,'cyc',cyc,'is_fix_C',false,'is_fix_A',true);
+    err_model3 = cross_valid_ldsi_uni(Y, xDim, nFold, timePoint, 'mean_type',mean_type,'tol',tol,'cyc',cyc,'is_fix_C',true,'is_fix_A',false);
+    err_model4 = cross_valid_ldsi_uni(Y, xDim, nFold, timePoint, 'mean_type',mean_type,'tol',tol,'cyc',cyc,'is_fix_C',false,'is_fix_A',false);
+
+    save([TempDatDir 'Session_' num2str(nSession) '.mat'],'err_model1','err_model2', 'err_model3', 'err_model4');
+    
+    figure;
+    Err_all    = 100 - [mean(err_model1');mean(err_model2');mean(err_model3');mean(err_model4')]*100;
+    Std_all    = [std(err_model1');std(err_model2');std(err_model3');std(err_model4')]/sqrt(nFold)*100;
+    errorbar((1:xDim)'*ones(1,4), Err_all',Std_all', '-o','linewid',1)
+    % plot(1:xDim, Err_all')
+    xlabel('Latent Dimension');
+    ylabel('% Exp. Var.');
+    xlim([0.5 xDim+1]);
+    ylim([0 ceil(max(Err_all(:)+Std_all(:)))])
+    box off
+    setPrint(6, 4.5, ['LDSPlots/LDSModelComparison_Session_' num2str(nSession)])
+end
---
+++ LiSimultaneousRecordings/fitTLDSoptDim.m
@@ -0,0 +1,43 @@
+addpath('../Func');
+addpath('../Release_LDSI_v3')
+setDir;
+
+load([TempDatDir 'Simultaneous_Spikes.mat'])
+mean_type    = 'Constant_mean';
+tol          = 1e-6;
+cyc          = 10000;
+timePoint    = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+timePoint    = timePoint(2:end-1);
+numSession   = length(nDataSet);
+xDimSet      = [2, 2, 4, 2, 3, 2, 4, 2;
+                0, 3, 0, 0, 4, 0, 0, 3];
+nFold        = 30;
+
+for nSession = 9:numSession
+    Y          = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+    Y          = permute(Y, [2 3 1]);
+    T          = size(Y, 2);
+    for nDim   = 1:size(xDimSet, 1)
+        xDim       = xDimSet(nDim, nSession);
+        if xDim>0
+            curr_err   = nan(nFold, 1);
+            for n_fold = 1:nFold
+                is_fit     = false;
+                while ~is_fit
+                    try
+                        Ph         = lds(Y, xDim, 'tol', tol, 'cyc', cyc, 'mean_type',mean_type, 'timePoint', timePoint);
+                        is_fit     = true;
+                    catch
+                        is_fit     = false;
+                    end
+                end
+                [curr_err(n_fold),~] = loo (Y, Ph, [0, timePoint, T]);
+                save([TempDatDir 'Session_' num2str(nSession) '_xDim' num2str(xDim) '_nFold' num2str(n_fold) '.mat'],'Ph');
+            end
+            [~, optFit] = min(curr_err);
+            disp([numSession, xDim, optFit])
+        end
+    end
+end
+
+close all
