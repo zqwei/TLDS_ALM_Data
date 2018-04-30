7d91a878d90057660cd2b05e00b6859eb6e7f254
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-04-30 14:28:59 -0400

    add analysis for Fig 3 and Fig s6, RT analysis

--- SimultaneousRecordings/firstTimeLickFullLDAScoreRankOrder.m
+++ SimultaneousRecordings/firstTimeLickFullLDAScoreRankOrder.m
@@ -2,10 +2,7 @@ addpath('../Func');
 setDir;
 
 load([TempDatDir 'Combined_Simultaneous_Spikes.mat'])
-analysisIndex = [1:8 18 26:41];
-nDataSet     = nDataSet(analysisIndex);
-timePoint    = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
-timePoint    = timePoint(2:end-1);
+load([TempDatDir 'Combined_data_SLDS_fit.mat'])
 numSession   = length(nDataSet);
 TLDS_decode  = nan(numSession, 4);
 LDA_decode   = nan(numSession, 4);
@@ -15,13 +12,20 @@ timeMax      = 600;
 timeMin      = 97;
 
 for nSession = 1:numSession
+    
+    param      = params(nDataSet(nSession).task_type);
+    timePoint    = timePointTrialPeriod(param.polein, param.poleout, param.timeSeries);
+    timePoint    = timePoint(2:end-1);
+    
     numYesTrial   = length(nDataSet(nSession).unit_yes_trial_index);
     numNoTrial    = length(nDataSet(nSession).unit_no_trial_index);
     totTargets    = nDataSet(nSession).totTargets;
     firstLickTime = nDataSet(nSession).firstLickTime;
     numUnits      = length(nDataSet(nSession).nUnit);
     numTrials     = numYesTrial + numNoTrial;
-    nSessionData  = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+%     nSessionData  = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+%     nSessionData  = [nDataSet(nSession).unit_KFFoward_yes_fit; nDataSet(nSession).unit_KFFoward_no_fit];
+    nSessionData  = fitData(nSession).K8yEst;
     nSessionData  = normalizationDim(nSessionData, 2);  
     nSessionBin   = mean(nSessionData(:, :, timePoint(end)+slideWin), 3);    
     coeffs        = coeffLDA(nSessionBin, totTargets);
@@ -33,7 +37,7 @@ for nSession = 1:numSession
     [p, h]        = corr(scoreMat(~totTargets & firstLickTime<timeMax & firstLickTime>timeMin), firstLickTime(~totTargets & firstLickTime<timeMax & firstLickTime>timeMin), 'type', 'Spearman');
     LDA_decode(nSession, [2 4]) = [p, h];
     
-    nSessionData  = [nDataSet(nSession).unit_yes_fit; nDataSet(nSession).unit_no_fit];
+    nSessionData  = [nDataSet(nSession).unit_KF_yes_fit; nDataSet(nSession).unit_KF_no_fit];
     nSessionData  = normalizationDim(nSessionData, 2);  
     nSessionBin   = mean(nSessionData(:, :, timePoint(end)+slideWin), 3);    
     coeffs        = coeffLDA(nSessionBin, totTargets);
@@ -60,15 +64,31 @@ end
 LDA_decode(24, 1) = 0.21;
 TLDS_decode(24, 1) = 0.45;
 
+figure;
+hold on
+plot(abs(LDA_decode(:, 1)), abs(TLDS_decode(:, 1)), 'ob')
+plot(abs(LDA_decode(:, 2)), abs(TLDS_decode(:, 2)), 'or')
+plot([0 1], [0 1], '--k')
+set(gca, 'TickDir', 'out')
+xlabel('RT')
+ylabel('RT')
+setPrint(8, 6, 'Plots/RTplot')
 
-contra_corr  = TLDS_decode(:, 1);
-ipsi_corr    = TLDS_decode(:, 2);
-contra_rtVar = nan(numSession, 1);
-ipsi_rtVar   = nan(numSession, 1);
-analysisIndex = [1:8 18 26:42];
-explainedCRR = [0.0941971391500321;0.216321097217030;0.127652483561879;0.198087983335957;0.357043607042852;0.0840192153190325;0.107332573346359;0.557599484041760;0.364319088067933;0.266487374589105;0.315822647980675;0.178577639473238;0.348245718708246;0.202200748283665;0.278196655269483;0.318707160797709;0.231859581027232;0.308891761309087;0.345569252518441;0.269017745000031;0.284241603848062;0.263076469867423;0.394405188775799;0.446577064694276;0.127320931929138;0.144772314850104;0.415913774401915;0.182525661848444;0.289938234354398;0.360483221402390;0.466216824758133;0.411115775925481;0.366425364730772;0.438203478685215;0.364274411208107;0.394978241906835;0.406057563091628;0.389362139537689;0.528165743352254;0.583359786983115;0.231085979647241;NaN]; % from EV_correct_error.m
-explainedCRR = explainedCRR(analysisIndex);
 
+
+[p, h] = signrank(abs(LDA_decode(:, 2)), abs(TLDS_decode(:, 2)))
+[p, h] = signrank(abs(LDA_decode(:, 1)), abs(TLDS_decode(:, 1)))
+
+
+
+
+
+
+% contra_corr  = TLDS_decode(:, 1);
+% ipsi_corr    = TLDS_decode(:, 2);
+% contra_rtVar = nan(numSession, 1);
+% ipsi_rtVar   = nan(numSession, 1);
+% 
 % 
 % for nSession = 1:numSession    
 %     firstLickTime = nDataSet(nSession).firstLickTime;
@@ -78,17 +98,17 @@ explainedCRR = explainedCRR(analysisIndex);
 %     contra_rtVar(nSession) = std(firstLickTime(totTargets));
 %     ipsi_rtVar(nSession)   = std(firstLickTime(~totTargets));    
 % end
-% 
+
 % 
 % 
 % filledStd = TLDS_decode(:, 3)<0.05; 
 % figure;
 % subplot(1, 2, 1)
 % hold on
-% scatter(contra_rtVar(~filledStd), contra_corr(~filledStd,1), [], explainedCRR(~filledStd))
-% scatter(contra_rtVar(filledStd), contra_corr(filledStd,1), [], explainedCRR(filledStd), 'filled')
-% plot([0 300], [0 0], '--k')
-% ylim([-0.5 0.5])
+% scatter(contra_rtVar(~filledStd), contra_corr(~filledStd,1))
+% scatter(contra_rtVar(filledStd), contra_corr(filledStd,1), 'filled')
+% % plot([0 300], [0 0], '--k')
+% % ylim([-0.5 0.5])
 % xlabel('Std Reaction time (ms)')
 % ylabel('Rank correlation RT-TLDS')
 % title('Contra')
--- SimultaneousRecordings/performanceLDA_early_late.m
+++ SimultaneousRecordings/performanceLDA_early_late.m
@@ -2,19 +2,21 @@ addpath('../Func');
 setDir;
 
 load([TempDatDir 'Combined_Simultaneous_Spikes.mat'])
-analysisIndex = [1:8 18 26:41];
-nDataSet     = nDataSet(analysisIndex);
-timePoint    = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
-timePoint    = timePoint(2:end-1);
+load([TempDatDir 'Combined_data_SLDS_fit.mat'])
 numSession   = length(nDataSet);
-TLDS_decode  = nan(numSession, 2);
+% TLDS_decode  = nan(numSession, 2);
 LDA_decode   = nan(numSession, 2);
-% explainedCRR = [0.0941971391500324;0.216321097217030;0.127652483561879;0.198087983335957;0.357043607042852;0.0840192153190327;0.107332573346359;0.557599484041760;0.127320931929138;0.144772314850104;0.415913774401915;0.182525661848444;0.289938234354398;0.360483221402390;0.466216824758133;0.411115775925481;0.366425364730772;0.438203478685215;0.364274411208107;0.394978241906835;0.406057563091628;0.389362139537689;0.528165743352254;0.583359786983115;0.231085979647241;NaN]; % from EV_correct_error.m
 kFold        = 10;
 slideWin     = 0;
 
 for nSession = 1:numSession
-    Y          = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+    param      = params(nDataSet(nSession).task_type);
+    timePoint    = timePointTrialPeriod(param.polein, param.poleout, param.timeSeries);
+    timePoint    = timePoint(2:end-1);
+    
+%     Y          = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
+%     Y          = [nDataSet(nSession).unit_KFFoward_yes_fit; nDataSet(nSession).unit_KFFoward_no_fit];
+    Y          = fitData(nSession).K8yEst;
     Y          = permute(Y, [2 3 1]);
     totTargets = nDataSet(nSession).totTargets;
     neuroMat   = squeeze(mean(Y(:, timePoint(end)+slideWin, :), 2));
@@ -25,20 +27,20 @@ for nSession = 1:numSession
     LDA_decode(nSession,1) = mean(L);
     LDA_decode(nSession,2) = std(L)/sqrt(kFold);
     
-    nSessionData  = [nDataSet(nSession).unit_yes_fit; nDataSet(nSession).unit_no_fit];
-    nSessionData  = normalizationDim(nSessionData, 2);  
-    coeffs        = coeffLDA(nSessionData, totTargets);
-    scoreMat      = nan(size(nSessionData, 1), size(nSessionData, 3));
-    for nTime     = 1:size(nSessionData, 3)
-        scoreMat(:, nTime) = squeeze(nSessionData(:, :, nTime)) * coeffs(:, nTime);
-    end
-    
-    neuroMat   = mean(scoreMat(:, timePoint(end)+slideWin), 2);
-    L          = 1 - kfoldLoss(fitcdiscr(neuroMat, totTargets, ...
-                 'discrimType','pseudoLinear', 'KFold', kFold),...
-                 'lossfun', 'classiferror', 'mode', 'individual');
-    TLDS_decode(nSession,1) = mean(L);
-    TLDS_decode(nSession,2) = std(L)/sqrt(kFold);
+%     nSessionData  = [nDataSet(nSession).unit_KF_yes_fit; nDataSet(nSession).unit_KF_no_fit];
+%     nSessionData  = normalizationDim(nSessionData, 2);  
+%     coeffs        = coeffLDA(nSessionData, totTargets);
+%     scoreMat      = nan(size(nSessionData, 1), size(nSessionData, 3));
+%     for nTime     = 1:size(nSessionData, 3)
+%         scoreMat(:, nTime) = squeeze(nSessionData(:, :, nTime)) * coeffs(:, nTime);
+%     end
+%     
+%     neuroMat   = mean(scoreMat(:, timePoint(end)+slideWin), 2);
+%     L          = 1 - kfoldLoss(fitcdiscr(neuroMat, totTargets, ...
+%                  'discrimType','pseudoLinear', 'KFold', kFold),...
+%                  'lossfun', 'classiferror', 'mode', 'individual');
+%     TLDS_decode(nSession,1) = mean(L);
+%     TLDS_decode(nSession,2) = std(L)/sqrt(kFold);
     
 end
 
@@ -48,6 +50,7 @@ hold on
 ploterr(LDA_decode(:,1), TLDS_decode(:,1), LDA_decode(:,2), TLDS_decode(:,2), '.k')
 % scatter(LDA_decode(:,1), TLDS_decode(:,1), [], explainedCRR, 'filled')
 scatter(LDA_decode(:,1), TLDS_decode(:,1), 'filled')
+[p, h] = signrank(LDA_decode(:,1), TLDS_decode(:,1))
 % colorbar
 plot([0.5 1], [0.5 1], '--k')
 xlim([0.5 1.001])
