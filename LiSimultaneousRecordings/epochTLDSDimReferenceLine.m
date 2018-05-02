9eabc3de77702e680e35b08223fbb1fd2bfe59eb
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-05-02 00:16:59 -0400

    update code with slds ev

--- LiSimultaneousRecordings/epochTLDSDimReferenceLine.m
+++ LiSimultaneousRecordings/epochTLDSDimReferenceLine.m
@@ -10,10 +10,8 @@ timePoint    = timePointTrialPeriod(params.polein, params.poleout, params.timeSe
 timePoint    = timePoint(2:end-1);
 numSession   = length(nDataSet);
 nFold        = 10;
-bestModelIdx = 4;
-thres        = 0.8;
-xDimSet      = [2, 3, 4, 2, 4, 2, 4, 3, 5, 3, 4, 5, 5, 6, 5, 5, 4, 4, 3, 3, 4, 6];
-optFitSet    = [6,10,11,10,30,18,19,27, 9,11, 9,30,13,11,30,25,11, 9,30,22, 1,15];
+% bestModelIdx = 4;
+% thres        = 0.8;
 
 GPFAresultsFolder = '/Volumes/My Drive/ALM_Recording_Pole_Task_Svoboda_Lab/TLDS_analysis_Li_data/GPFA_Li_Data/mat_results/';
 
@@ -23,7 +21,7 @@ for nSession = 17%1:numSession % example session 17
     figure;
     Err_all    = 100 - [mean(err_model1');mean(err_model2');mean(err_model3');mean(err_model4')]*100;
     Std_all    = [std(err_model1');std(err_model2');std(err_model3');std(err_model4')]/sqrt(nFold)*100;    
-    [maxEV, maxIdx] = max(Err_all(bestModelIdx, :));
+%     [maxEV, maxIdx] = max(Err_all(bestModelIdx, :));
 %     optIdx     = find(Err_all(bestModelIdx, :)>maxEV*thres, 1, 'first');
     hold on
     errorbar((1:xDim)'*ones(1,4), Err_all',Std_all', '-o','linewid',1)
@@ -44,18 +42,16 @@ for nSession = 17%1:numSession % example session 17
     GPFAErr    = GPFAErr * 100;
     errorbar(1:xDim, mean(GPFAErr), std(GPFAErr), '-o','linewid',1)
     
+    % mean
     Y          = [nDataSet(nSession).unit_yes_trial; nDataSet(nSession).unit_no_trial];
-    yesTrial   = size(nDataSet(nSession).unit_yes_trial, 1);
-    noTrial    = size(nDataSet(nSession).unit_no_trial, 1);
-    Y          = permute(Y, [2 3 1]);
-    yDim       = size(Y, 1);
-    T          = size(Y, 2);
-    totTrial   = [true(yesTrial, 1); false(noTrial, 1)];
-    xDim       = xDimSet(nSession);
-    optFit     = optFitSet(nSession);
-    load ([TempDatDir 'Session_' num2str(nSession) '_xDim' num2str(xDim) '_nFold' num2str(optFit) '.mat'],'Ph');    
-    err        = evMean (Y, Ph, [0, timePoint, T], totTrial)*100;
-
+    Y_s        = permute(Y, [2 3 1]); % unit x time x trial
+    numUnit    = size(Y_s, 1);
+    Y_s        = reshape(Y_s, numUnit, []);
+    mean_y     = mean(mean(Y_s, 2),3);
+    rand_y     = sum(remove_mean(Y_s, mean_y).^2, 2);
+    yes_var    = sum(squeeze(var(nDataSet(nSession).unit_yes_trial, [], 1))*size(nDataSet(nSession).unit_yes_trial, 1), 2);
+    no_var     = sum(squeeze(var(nDataSet(nSession).unit_no_trial, [], 1))*size(nDataSet(nSession).unit_no_trial, 1), 2);
+    err = 1 - mean((yes_var+no_var)./rand_y);
     xlabel('Latent Dimension');
     ylabel('% Exp. Var.');
     gridxy([], [100-err],'Color','k','Linestyle','--')
