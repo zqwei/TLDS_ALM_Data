30000b8397d9560210dbe2505d57754b2e413ed8
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-06-17 21:02:47 -0400

    add code to behaviors of predicition of previous trials

--- SimultaneousRecordings/behavioralRankDynamics.m
+++ SimultaneousRecordings/behavioralRankDynamics.m
@@ -17,9 +17,31 @@ for nData = 17 %1:length(nDataSet)
     
     trial_index   = [nDataSet(nData).unit_yes_trial_index, nDataSet(nData).unit_no_trial_index];
     
-    early_trial   = nan(numTrials);
-    reward_trial  = nan(numTrials);
+%     early_trial   = nan(numTrials);
+%     reward_trial  = nan(numTrials);
+%     stim_trial    = nan(numTrials);
     
+    wind          = 3;
+    ave_trial_ = movmean(nBehaviors(nData).behavior_early_report, wind*2+1);
+    ave_trial  = nBehaviors(nData).behavior_early_report;
+    ave_trial(wind+1:end) = ave_trial_(1:end-wind);
+    early_trial   = ave_trial(trial_index);
+    
+    reward        = double(nBehaviors(nData).behavior_early_report==0 & nBehaviors(nData).behavior_report==1);
+    ave_trial_    = movmean(reward, wind*2+1);
+    ave_trial     = reward;
+    ave_trial(wind+1:end) = ave_trial_(1:end-wind);
+    reward_trial  = ave_trial(trial_index);
+    
+    if size(nBehaviors(nData).task_stimulation, 1)>1
+        stim      = double(~nBehaviors(nData).task_stimulation(:,1)==0);
+    else
+        stim      = double(~nBehaviors(nData).task_stimulation==0);
+    end
+    ave_trial_    = movmean(stim, wind*2+1);
+    ave_trial     = stim;
+    ave_trial(wind+1:end) = ave_trial_(1:end-wind);
+    stim_trial    = ave_trial(trial_index);
     
     % KF
     nSessionData  = [nDataSet(nData).unit_KF_yes_fit; nDataSet(nData).unit_KF_no_fit];
@@ -32,11 +54,17 @@ for nData = 17 %1:length(nDataSet)
         scoreMat(:, nTime) = squeeze(nSessionData(:, :, nTime)) * coeffs(:, nTime);
     end
     
+    figure
     trialKFCorrEpoch = nan(2, length(timePoint)-1);
+    group            = [ones(numYesTrial, 1); ones(numTrials-numYesTrial, 1)*2];
     for nEpoch     = 1:length(timePoint)-1
-        scoreMat_  = scoreMat(:, timePoint(nEpoch):timePoint(nEpoch+1));
-%         trialKFCorrEpoch(1, nEpoch) = mean(corr(scoreMat_(1:numYesTrial, :), (1:numYesTrial)', 'type', 'spearman'));
-%         trialKFCorrEpoch(2, nEpoch) = mean(corr(scoreMat_(1+numYesTrial:end,:), (1:(numTrials-numYesTrial))', 'type', 'spearman'));
+        scoreMat_  = mean(scoreMat(:, timePoint(nEpoch):timePoint(nEpoch+1)), 2);
+        subplot(4, 3, (nEpoch-1)*3 + 1)
+        scatter(early_trial, scoreMat_, [], group)
+        subplot(4, 3, (nEpoch-1)*3 + 2)
+        scatter(reward_trial, scoreMat_, [], group)
+        subplot(4, 3, (nEpoch-1)*3 + 3)
+        scatter(stim_trial, scoreMat_, [], group)
     end
 
 end
---
+++ SimultaneousRecordings/behavioralRankDynamics_v2.m
@@ -0,0 +1,118 @@
+addpath('../Func');
+addpath('../Release_LDSI_v3');
+setDir;
+
+load([TempDatDir 'Combined_Simultaneous_Spikes.mat'])
+load([TempDatDir 'Combined_Simultaneous_Behaviors.mat'])
+load([TempDatDir 'Combined_data_SLDS_fit.mat'])
+
+
+param               = params(nDataSet(1).task_type);
+sigma               = 0.15 / param.binsize; % 300 ms
+filterLength        = 11;
+filterStep          = linspace(-filterLength / 2, filterLength / 2, filterLength);
+filterInUse         = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
+filterInUse         = filterInUse / sum (filterInUse); 
+
+ave_reward          =  nan(length(nDataSet), 8);
+
+for nData = 1:length(nDataSet)
+    param          = params(nDataSet(nData).task_type);
+    timePoint      = timePointTrialPeriod(param.polein, param.poleout, param.timeSeries);
+    
+    totTargets    = nDataSet(nData).totTargets;
+    numUnits      = length(nDataSet(nData).nUnit);
+    numTrials     = length(totTargets);
+    numYesTrial   = sum(totTargets);
+    
+    if iscolumn(nDataSet(nData).unit_yes_trial_index)
+        trial_index   = [nDataSet(nData).unit_yes_trial_index', nDataSet(nData).unit_no_trial_index'];
+    else
+        trial_index   = [nDataSet(nData).unit_yes_trial_index, nDataSet(nData).unit_no_trial_index];
+    end
+    
+    early_trial   = nBehaviors(nData).behavior_early_report(max(trial_index-1, 1));
+    reward        = nBehaviors(nData).behavior_early_report==0 & nBehaviors(nData).behavior_report==1;
+    reward_trial  = reward(max(trial_index-1, 1));
+    if size(nBehaviors(nData).task_stimulation, 1)>1
+        stim      = double(~(nBehaviors(nData).task_stimulation(:,1)==0));
+    else
+        stim      = double(~(nBehaviors(nData).task_stimulation==0));
+    end
+    stim_trial    = stim(max(trial_index-1, 1));
+    trial_type    = nBehaviors(nData).task_trial_type(max(trial_index-1, 1))==1;
+    
+    % KF
+    nSessionData  = [nDataSet(nData).unit_KF_yes_fit; nDataSet(nData).unit_KF_no_fit];
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate   = coeffSVM(nSessionData, reward_trial);    
+    ave_reward(nData, 1) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %Full space
+    nSessionData  = [nDataSet(nData).unit_yes_trial; nDataSet(nData).unit_no_trial];
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 2) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    
+    %CKF
+    nSessionData  = [nDataSet(nData).unit_CKF_yes_fit; nDataSet(nData).unit_CKF_no_fit];
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 3) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %GPFA
+    nSessionData  = [nDataSet(nData).unit_GPFA_yes_fit; nDataSet(nData).unit_GPFA_no_fit];
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 4) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %KF forward
+    nSessionData  = [nDataSet(nData).unit_KFFoward_yes_fit; nDataSet(nData).unit_KFFoward_no_fit];
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 5) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %KS2
+    nSessionData  = fitData(nData).K2yEst;
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 6) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %KS4
+    nSessionData  = fitData(nData).K4yEst;
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 7) = mean(correctRate(1, timePoint(1):timePoint(2)));
+    
+    %KS8
+    nSessionData  = fitData(nData).K8yEst;
+    nSessionData  = normalizationDim(nSessionData, 2); 
+    correctRate = coeffSVM(nSessionData, reward_trial);
+    ave_reward(nData, 8) = mean(correctRate(1, timePoint(1):timePoint(2)));
+
+end
+
+
+figure;
+xlabels = {'Full space', 'LDS', 'GPFA',  'KF forward', 'sLDS2', 'sLDS4', 'sLDS8'};
+tot_plot = 0;
+for nPlot = 1:7
+    tot_plot = tot_plot + 1;
+    subplot(1, 7, tot_plot)
+    hold on
+    plot(ave_reward(:, 1), ave_reward(:, nPlot+1), 'ob')
+    p = signrank(ave_reward(:, 1), ave_reward(:, nPlot+1), 'tail', 'both')
+    plot([0 1], [0, 1], '--k')
+%     xlim([0 0.6])
+%     ylim([0 0.6])
+    xlabel('SAS')
+    ylabel(xlabels{nPlot})
+%     if p > 0.001
+%         disp([epoch_titles{nEpoch} ' ' xlabels{nPlot} ' ' num2str(p)])
+%     end
+%         title([epoch_titles{nEpoch} 'p=' num2str(p, '%.3f')])
+    set(gca, 'TickDir', 'out')
+end
+
+setPrint(8*4, 6*7, 'Plots/NSession_comparison_previous_trial_Fits', 'pdf')
