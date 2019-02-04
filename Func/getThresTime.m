f462478834acf0a1331d68f9059c2bbab846d58b
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-02-04 13:13:29 -0500

    remove old unrelated files

--- Func/getThresTime.m
+++
@@ -1,8 +0,0 @@
-function currTime = getThresTime(y, thresValue, minCurrTime, maxCurrTime)
-    
-    currTimeSet   = find(y > thresValue);
-    if ~isempty(currTimeSet)
-        currTime  = min(currTimeSet(currTimeSet>minCurrTime));
-    else
-        currTime  = maxCurrTime;
-    end
